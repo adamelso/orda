7700505c6c466b1e3db8feb63958724fd0f2eaf8
Adam Elsodaney <adam.elso@gmail.com>
2016-03-11 08:53:37 +0000

    Commit all the things, don't be late for work

---
+++ README.md
@@ -0,0 +1,4 @@
+orderly
+=======
+
+A Symfony project created on March 11, 2016, 9:33 am.
---
+++ src/AppBundle/Entity/Order.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Sylius\Component\Order\Model\Order as SyliusOrder;
+
+class Order extends SyliusOrder
+{
+
+}
